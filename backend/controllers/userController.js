ebf8d46ac7673b80500b3b1706ae7e153b0cc809
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-04-24 21:02:01 +0700

    v2-4

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -38,7 +38,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
-    //   token: generateToken(user._id),
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -61,7 +61,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
-    //   token: generateToken(user._id),
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -73,16 +73,20 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message:'C'})
-//   res.status(200).json(req.user)
+  const {_id,name,email} = await User.findById(req.user.id)
+  res.status(200).json({
+      id: _id,
+      name,
+      email,
+  })
 })
 
 // Generate JWT
-// const generateToken = (id) => {
-//   return jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: '30d',
-//   })
-// }
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
