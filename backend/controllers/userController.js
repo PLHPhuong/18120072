7045292b6c182c259ab513f6cf5b9887971fffdc
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-04-24 20:37:29 +0700

    sad

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
-// const jwt = require('jsonwebtoken')
-// const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
@@ -7,67 +7,66 @@ const User = require('../models/userModel')
 // @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({message:'A'})
-//   const { name, email, password } = req.body
+  const { name, email, password } = req.body
 
-//   if (!name || !email || !password) {
-//     res.status(400)
-//     throw new Error('Please add all fields')
-//   }
+  if (!name || !email || !password) {
+    res.status(400)
+    throw new Error('Please add all fields')
+  }
 
-//   // Check if user exists
-//   const userExists = await User.findOne({ email })
+  // Check if user exists
+  const userExists = await User.findOne({ email })
 
-//   if (userExists) {
-//     res.status(400)
-//     throw new Error('User already exists')
-//   }
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
 
-//   // Hash password
-//   const salt = await bcrypt.genSalt(10)
-//   const hashedPassword = await bcrypt.hash(password, salt)
+  // Hash password
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
 
-//   // Create user
-//   const user = await User.create({
-//     name,
-//     email,
-//     password: hashedPassword,
-//   })
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
 
-//   if (user) {
-//     res.status(201).json({
-//       _id: user.id,
-//       name: user.name,
-//       email: user.email,
-//       token: generateToken(user._id),
-//     })
-//   } else {
-//     res.status(400)
-//     throw new Error('Invalid user data')
-//   }
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    //   token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
 })
 
 // @desc    Authenticate a user
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message:'B'})
-//   const { email, password } = req.body
+  const { email, password } = req.body
 
-//   // Check for user email
-//   const user = await User.findOne({ email })
+  // Check for user email
 
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     res.json({
-//       _id: user.id,
-//       name: user.name,
-//       email: user.email,
-//       token: generateToken(user._id),
-//     })
-//   } else {
-//     res.status(400)
-//     throw new Error('Invalid credentials')
-//   }
+  const user = await User.findOne({ email })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    //   token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid credentials')
+  }
 })
 
 // @desc    Get user data
