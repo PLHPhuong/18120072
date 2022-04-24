dc1c11caba9922a36c3af25571daf9da4aef553a
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-04-24 13:33:06 +0700

    Goals controller

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,16 @@
+const asyncHandler = require('express-async-handler')
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req,res) =>{
+const getGoals = asyncHandler(async (req,res) =>{
     res.status(200).json({message: 'Get goals'})
-}
+})
 
 // @desc Set goals
 // @route POST /api/goals
 // @access Private
-const setGoals = (req,res) =>{
+const setGoals = asyncHandler(async (req,res) =>{
     // in video 23:47 - !req.body.text 
     // but since req.body.text mean they have key that is text
     // use alternative from https://stackoverflow.com/questions/42921727/how-to-check-req-body-empty-or-not-in-node-express
@@ -19,21 +21,21 @@ const setGoals = (req,res) =>{
         throw new Error('Please add a text field')
     }
     res.status(200).json({message:'Set goals'})
-}
+})
 
 // @desc Update goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = (req,res) =>{
+const updateGoal = asyncHandler (async (req,res) =>{
     res.status(200).json({message:`Update goal ${req.params.id}`})
-}
+})
 
 // @desc Delete goals
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = (req,res) =>{
+const deleteGoal = asyncHandler (async (req,res) =>{
     res.status(200).json({message:`Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
     getGoals,
