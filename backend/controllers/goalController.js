5ec91d9caaac7b99d14a8016fb61e3d58003b067
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-04-24 17:34:16 +0700

    V1

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,40 +1,74 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = asyncHandler(async (req,res) =>{
-    res.status(200).json({message: 'Get goals'})
+const getGoals = asyncHandler(async (req, res) => {
+    const goals = await Goal.find()
+
+    res.status(200).json(goals)
 })
 
 // @desc Set goals
 // @route POST /api/goals
 // @access Private
-const setGoals = asyncHandler(async (req,res) =>{
+const setGoals = asyncHandler(async (req, res) => {
     // in video 23:47 - !req.body.text 
     // but since req.body.text mean they have key that is text
-    // use alternative from https://stackoverflow.com/questions/42921727/how-to-check-req-body-empty-or-not-in-node-express
-    // if (!req.body.text) {
-    if (!Object.keys(req.body).length) {
+    // can use alternative from https://stackoverflow.com/questions/42921727/how-to-check-req-body-empty-or-not-in-node-express
+    // for universal purpose
+    if (!req.body.text) {
+        // if (!Object.keys(req.body).length) {
         // res.status(400).json({message:'pls add a text field'})
         res.status(400)
         throw new Error('Please add a text field')
     }
-    res.status(200).json({message:'Set goals'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+    res.status(200).json({
+        message: 'Set goals'
+    })
 })
 
 // @desc Update goals
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = asyncHandler (async (req,res) =>{
-    res.status(200).json({message:`Update goal ${req.params.id}`})
+const updateGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+    // res.status(200).json({
+    //     message: `Update goal ${req.params.id}`
+    // })
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goals
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = asyncHandler (async (req,res) =>{
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
+const deleteGoal = asyncHandler(async (req, res) => {
+    const goal = await Goal.findById(req.params.id)
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    await goal.remove()
+
+    // res.status(200).json({
+    //     message: `Delete goal ${req.params.id}`
+    // })
+    res.status(200).json({
+        id: req.params.id
+    })
+
 })
 
 module.exports = {
