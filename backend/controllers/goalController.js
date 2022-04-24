30533a47f66d287aa9221b6f8f4409cf8da71b76
PLHPhuong <64248912+PLHPhuong@users.noreply.github.com>
2022-04-24 07:21:22 +0700

    2

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -6,11 +6,36 @@ const getGoals = (req,res) =>{
 }
 
 // @desc Set goals
-// @route PUT /api/goals
+// @route POST /api/goals
 // @access Private
-const SetGoals = (req,res) =>{
-    res.status(200).json({message: 'Get goals'})
+const setGoals = (req,res) =>{
+    // in video 23:47 - !req.body.text
+    console.log(req.body)
+    if (!req.body.text()) {
+        res.status(400).json({message:'pls add a text field'})
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
+    res.status(200).json({message:'Set goals'})
 }
+
+// @desc Update goals
+// @route PUT /api/goals/:id
+// @access Private
+const updateGoal = (req,res) =>{
+    res.status(200).json({message:`Update goal ${req.params.id}`})
+}
+
+// @desc Delete goals
+// @route DELETE /api/goals/:id
+// @access Private
+const deleteGoal = (req,res) =>{
+    res.status(200).json({message:`Delete goal ${req.params.id}`})
+}
+
 module.exports = {
     getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,19 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const { getGoals} = require('../controllers/goalController')
+const {
+    getGoals,
+    setGoals,
+    updateGoal,
+    deleteGoal
+} = require('../controllers/goalController')
+
+
+router.route('/').get(getGoals).post(setGoals)
+router.route('/:id').put(updateGoal).delete(deleteGoal)
+
 module.exports = router
-
-
-router.get('/',getGoals)
-
-router.post('/',(req,res) => {
-    res.status(200).json({message:'Set goals'})
-})
-
-router.put('/:id',(req,res) => {
-    res.status(200).json({message:`Update goal ${req.params.id}`})
-})
-
-router.delete('/:id',(req,res) => {
-    res.status(200).json({message:`Delete goal ${req.params.id}`})
-})
